acb57f9459fde12f17ff1bf17feff2b08e4981eb
Feng Tian <fengttt@gmail.com>
2015-04-04 09:42:34 -0700

    Build.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 MY_TOOLCHAIN_DIR=$PWD
+# (cd snappy-1.1.2; ./configure --prefix=$MY_TOOLCHAIN_DIR/installed; make; make install)
 # (cd gperftools-2.4; ./configure --prefix=$MY_TOOLCHAIN_DIR/installed --enable-frame-pointers; make; make install)
 echo "Making lz4 ..."
 (cd lz4-r127/lib; make; PREFIX=$MY_TOOLCHAIN_DIR/installed make install)
@@ -6,3 +7,5 @@ echo "Making rapidjson ..."
 (cp -r rapidjson/include/rapidjson $MY_TOOLCHAIN_DIR/installed/include)
 echo "Making gtest ..." 
 (cd gtest-1.7.0; ./configure; make)
+echo "Making protobuf ..."
+(cd protobuf-2.6.1; ./configure --prefix=$MY_TOOLCHAIN_DIR/installed; make; make install)
