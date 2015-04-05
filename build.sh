00f335efd85ce97e0adaad427ac52ae639e6a916
Feng Tian <fengttt@gmail.com>
2015-04-05 11:24:04 -0700

    Build script.

--- build.sh
+++ build.sh
@@ -9,3 +9,7 @@ echo "Making gtest ..."
 (cd gtest-1.7.0; ./configure; make)
 echo "Making protobuf ..."
 (cd protobuf-2.5.0; ./configure --prefix=$MY_TOOLCHAIN_DIR/installed; make; make install)
+echo "Making gflags ..."
+(cd gflags-2.1.2; cmake -DCMAKE_INSTALL_PREFIX=$MY_TOOLCHAIN_DIR/installed .; make; make install)
+echo "Making glog ..."
+(cd glog-0.3.4; ./configure --prefix=$MY_TOOLCHAIN_DIR/installed; make; make install)
