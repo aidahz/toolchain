43c09b4575b99cbcad08d7c04e6b22f3cbf0343f
Feng Tian <fengttt@gmail.com>
2015-05-01 09:34:46 -0700

    Add thrift.

--- gtest-1.7.0/CMakeFiles/gtest_main.dir/DependInfo.cmake
+++ gtest-1.7.0/CMakeFiles/gtest_main.dir/DependInfo.cmake
@@ -1,23 +1,23 @@
 # The set of languages for which implicit dependencies are needed:
-set(CMAKE_DEPENDS_LANGUAGES
+SET(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
+SET(CMAKE_DEPENDS_CHECK_CXX
   "/nx/work/toolchain/gtest-1.7.0/src/gtest_main.cc" "/nx/work/toolchain/gtest-1.7.0/CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
   )
-set(CMAKE_CXX_COMPILER_ID "Clang")
+SET(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+SET(CMAKE_TARGET_LINKED_INFO_FILES
   "/nx/work/toolchain/gtest-1.7.0/CMakeFiles/gtest.dir/DependInfo.cmake"
   )
 
 # The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
+SET(CMAKE_C_TARGET_INCLUDE_PATH
   "include"
   "."
   )
-set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
-set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
