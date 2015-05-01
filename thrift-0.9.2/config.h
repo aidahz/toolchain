093406589bfb23dc27b704ae49a1dabf962f04d8
Feng Tian <fengttt@gmail.com>
2015-05-01 10:16:53 -0700

    Adding boost     Just cannot fight off the evil.

--- thrift-0.9.2/config.h
+++ thrift-0.9.2/config.h
@@ -13,13 +13,13 @@
 #define ARITHMETIC_RIGHT_SHIFT 1
 
 /* Defines automake version */
-#define AUTOMAKE_VERSION 1.14.1
+#define AUTOMAKE_VERSION 1.13.4
 
 /* Use *.h extension for parser header file */
 /* #undef BISON_USE_PARSER_H_EXTENSION */
 
 /* Defines bison version */
-#define BISON_VERSION 3.0.2
+#define BISON_VERSION 2.7
 
 /* Define to one of `_getb67', `GETB67', `getb67' for Cray-2 and Cray-YMP
    systems. This function is required for `alloca.c' support on those systems.
@@ -43,7 +43,7 @@
 #define HAVE_ARPA_INET_H 1
 
 /* define if the Boost library is available */
-#define HAVE_BOOST /**/
+/* #undef HAVE_BOOST */
 
 /* Define to 1 if you have the `bzero' function. */
 #define HAVE_BZERO 1
@@ -357,7 +357,7 @@
 
 /* Define to 1 if `lex' declares `yytext' as a `char *' by default, not a
    `char[]'. */
-#define YYTEXT_POINTER 1
+/* #undef YYTEXT_POINTER */
 
 /* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
    <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
