ccd2ff848c4df4ae938b196e1030a68e9d8bd767
CK Tan <cktan@vitessedata.com>
2016-08-16 12:39:27 -0700

    fix libhdfs3/src/Makefile

--- libhdfs3/src/src.mk
+++ libhdfs3/src/src.mk
@@ -1,9 +1,9 @@
 CXXFLAGS += -I$(srcdir)/../../installed/include
 CXXFLAGS += -I$(srcdir)/../../installed/include/libxml2
 CXXFLAGS += -I$(srcdir)/common -I.. -I../proto
-CXXFLAGS += -Wall -msse4.2 -std=c++11 
-#CXXFLAGS += -O0
-CXXFLAGS += -O2 -g -DNDEBUG
+CXXFLAGS += -Wall -msse4.2 -std=c++11 -g
+CXXFLAGS += -O0
+#CXXFLAGS += -O2 -DNDEBUG
 
 all: $(OBJS)
 	ld -r $(OBJS) -o subobj.o
