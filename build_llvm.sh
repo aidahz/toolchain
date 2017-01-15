7b909e39b166fa3947fa50bb65a815b8b11172ff
CK Tan <cktan@vitessedata.com>
2017-01-14 23:08:18 -0800

    add llvm

---
+++ build_llvm.sh
@@ -0,0 +1,62 @@
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MY_TOOLCHAIN_DIR=$DIR
+
+
+function fatal
+{
+   echo
+   echo ERROR: "$@"
+   echo
+   exit 1
+}
+
+
+STARTTIME=0
+function start
+{
+   echo -n $1
+   STARTTIME=$(date +%s)
+}
+
+
+function pass 
+{
+   ENDTIME=$(date +%s)
+   ELAPSED=$(($ENDTIME - $STARTTIME))
+   echo "[ok  $ELAPSED sec]"
+}
+
+function fail
+{
+   echo '[fail]'
+   if [ $1 ]; then echo "$@"; fi
+}
+
+
+##########################
+start 'llvm debug: .....'
+# NB: this is actually a Release build with assertions ON
+(rm -rf llvm-3.9.1.src && tar xf llvm-3.9.1.src.tar.xz && cd llvm-3.9.1.src \
+	&& rm -rf build \
+	&& mkdir build \
+	&& cd build \
+	&& cmake -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN_DIR/installed/llvm-debug \
+		-DCMAKE_BUILD_TYPE=Release \
+		-DLLVM_ENABLE_ASSERTIONS=On \
+		.. \
+	&& (make -j8  || make -j8 || make) \
+	&& make install) >& out/llvm-debug.out && pass || fail
+
+##########################
+start 'llvm release: ...'
+(rm -rf llvm-3.9.1.src && tar xf llvm-3.9.1.src.tar.xz && cd llvm-3.9.1.src \
+	&& rm -rf build \
+	&& mkdir build \
+	&& cd build \
+	&& cmake -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$TOOLCHAIN_DIR/installed/llvm-release \
+		-DCMAKE_BUILD_TYPE=Release \
+		.. \
+	&& (make -j8  || make -j8 || make) \
+	&& make install) >& out/llvm-release.out && pass || fail
