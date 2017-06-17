c7d4a349d366e5d0fdc08a29f68cf5ba0eef1744
CK Tan <cktan@vitessedata.com>
2017-06-17 18:24:52 +0800

    Clean up gmock and gtest links during build

--- build.sh
+++ build.sh
@@ -57,8 +57,10 @@ start 'tomlc99: ........'
 start 'protobuf: .......'
 (rm -rf protobuf-3.0.0-GA && unzip protobuf-3.0.0-GA.zip \
 	&& unzip -q gmock.zip \
+	&& rm -f protobuf-3.0.0-GA/gmock \
         && mv googlemock-release-1.7.0 protobuf-3.0.0-GA/gmock \
 	&& unzip -q gtest.zip \
+	&& rm -f protobuf-3.0.0-GA/gmock \
         && mv googletest-release-1.7.0 protobuf-3.0.0-GA/gmock/gtest \
 	&& cd protobuf-3.0.0-GA  \
 	&& ./autogen.sh \
