171eaed4bf85567db0c8adf171cdc2f1b640ffe8
cktan <cktan@vitessedata.com>
2017-06-22 00:49:47 +0000

    Minor fix to build.sh

--- build.sh
+++ build.sh
@@ -57,10 +57,10 @@ start 'tomlc99: ........'
 start 'protobuf: .......'
 (rm -rf protobuf-3.0.0-GA && unzip -o protobuf-3.0.0-GA.zip \
 	&& unzip -o -q gmock.zip \
-	&& rm -f protobuf-3.0.0-GA/gmock \
+	&& rm -rf protobuf-3.0.0-GA/gmock \
         && mv googlemock-release-1.7.0 protobuf-3.0.0-GA/gmock \
 	&& unzip -o -q gtest.zip \
-	&& rm -f protobuf-3.0.0-GA/gmock \
+	&& rm -rf protobuf-3.0.0-GA/gmock \
         && mv googletest-release-1.7.0 protobuf-3.0.0-GA/gmock/gtest \
 	&& cd protobuf-3.0.0-GA  \
 	&& ./autogen.sh \
