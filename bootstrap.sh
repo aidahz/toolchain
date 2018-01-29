da43b4e3fad3a3af1549ed2ce7cd8553834c147d
Feng Tian <fengttt@gmail.com>
2018-01-29 01:01:08 -0800

    Update scripts to use bootstrapped aws.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,7 @@
 (rm -rf grpc && git clone -b v1.7.x https://github.com/grpc/grpc \
 	&& cd grpc \
 	&& git submodule update --init )
+
+(rm -fr aws-sdk-cpp && git clone https://github.com/aws/aws-sdk-cpp \
+	&& cd aws-sdk-cpp \
+	&& git checkout tags/1.3.48 )
