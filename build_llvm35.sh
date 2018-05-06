ab48d0e6a393bd7a51a610ceab98adc189b94dbf
CK Tan <cktan@vitessedata.com>
2018-05-06 00:34:03 -0700

    edit

---
+++ build_llvm35.sh
@@ -0,0 +1,24 @@
+set -e 
+tar xJf llvm-3.5.2.src.tar.xz
+ln -s llvm-3.5.2.src llvm
+
+sudo mkdir -p /opt/llvm-release+assert
+sudo chown $USER /opt/llvm-release+assert
+rm -rf build
+mkdir -p build
+(cd build &&
+   ../llvm/configure --prefix=/opt/llvm-release+assert \
+	--enable-targets=x86_64 --enable-optimized=YES &&
+   make clean && make -j8 && sudo make install)
+
+
+
+sudo mkdir -p /opt/llvm-release
+sudo chown $USER /opt/llvm-release
+rm -rf build
+mkdir -p build
+(cd build && 
+    ../llvm/configure --prefix=/opt/llvm-release \
+	--enable-targets=x86_64 --enable-optimized=YES --enable-assertions=NO &&
+    make clean && make -j8 && sudo make install)
+
