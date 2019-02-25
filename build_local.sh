88c7587690197bbc10f2b6c7247a8c66255eba92
Feng Tian <fengttt@gmail.com>
2019-02-25 09:16:19 -0800

    Build exx before sparquet.

--- build_local.sh
+++ build_local.sh
@@ -46,14 +46,14 @@ echo -n 'viper: ..........'
         make -j8 && make install prefix="$TARGETDIR" ) >& out/viper.out && pass || fail
 
 ##########################
-echo -n 'sparquet: .......'
-(cd mendota/sparquet && make clean &&
-        make && make install prefix="$TARGETDIR" ) >& out/sparquet.out && pass || fail
-
-##########################
 echo -n 'exx: ............'
 (cd mendota/exx && make clean && make -j8 &&
 	make install prefix=$TARGETDIR ) >& out/exx && pass || fail
+
+##########################
+echo -n 'sparquet: .......'
+(cd mendota/sparquet && make clean &&
+        make && make install prefix="$TARGETDIR" ) >& out/sparquet.out && pass || fail
 
 ##########################
 echo -n 'libhdfs3: .......'
