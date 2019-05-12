382ba3ad9e4431b4a13ab0ba28d20c82ca6d91a5
CK Tan <cktan@vitessedata.com>
2019-05-12 13:03:59 -0700

    Revert "fix"
    This reverts commit 4eb4f5db1a294d268f2de87bb7a62439a6653d91.

--- build.sh
+++ build.sh
@@ -66,12 +66,11 @@ start 'tomlc99: ........'
 
 ##########################
 start 'event: ..........'
-rm -rf libevent-2.0.22-stable
 HINT=
 if [ -e /opt/local/include/openssl ] && ! [ -e /usr/local/include/openssl ] ; then
     HINT='HINT: try (cd /usr/local/include; ln -s /opt/local/include/openssl)'
 fi
-(tar xvfz libevent-2.0.22-stable.tar.gz && cd libevent-2.0.22-stable && ./configure --prefix=$TARGETDIR && make clean && make -j8 && make install) >& out/event.out && pass || fail $HINT
+(cd libevent-2.0.22-stable && ./configure --prefix=$TARGETDIR && make clean && make -j8 && make install) >& out/event.out && pass || fail $HINT
 
 ##########################
 # start 'cmake: ..........'
