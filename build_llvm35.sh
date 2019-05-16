7bb5131b5ac36526e5877ffa0a7de9d2c87b04ff
CK Tan <cktan@vitessedata.com>
2019-05-16 16:52:03 -0700

    fix target of llvm35 build

--- build_llvm35.sh
+++ build_llvm35.sh
@@ -14,25 +14,24 @@ rm -f llvm
 tar xJf llvm-3.5.2.src.tar.xz
 ln -f -s llvm-3.5.2.src llvm
 
-sudo rmdir /opt/llvm-release+assert
-sudo mkdir -p /opt/llvm-release+assert
-sudo chown $USER /opt/llvm-release+assert
+sudo rm -rf /opt/llvm35-release+assert
+sudo mkdir -p /opt/llvm35-release+assert
+sudo chown $USER /opt/llvm35-release+assert
 rm -rf build
 mkdir -p build
 (cd build &&
-   ../llvm/configure --prefix=/opt/llvm-release+assert \
+   ../llvm/configure --prefix=/opt/llvm35-release+assert \
 	--enable-targets=x86_64 --enable-optimized=YES &&
    make clean && make -j8 && sudo make install) || fatal llvm-release+assert
 
 
-
-sudo rmdir /opt/llvm-release
-sudo mkdir -p /opt/llvm-release
-sudo chown $USER /opt/llvm-release
+sudo rm -rf /opt/llvm35-release
+sudo mkdir -p /opt/llvm35-release
+sudo chown $USER /opt/llvm35-release
 rm -rf build
 mkdir -p build
 (cd build && 
-    ../llvm/configure --prefix=/opt/llvm-release \
+    ../llvm/configure --prefix=/opt/llvm35-release \
 	--enable-targets=x86_64 --enable-optimized=YES --enable-assertions=NO &&
     make clean && make -j8 && sudo make install) || fatal llvm-release
  
--- build_llvm35_opt.sh
+++ build_llvm35_opt.sh
@@ -1,8 +1,4 @@
 set -e 
-rm -rf llvm-3.5.2.src
-rm -f llvm
-tar xJf llvm-3.5.2.src.tar.xz
-ln -f -s llvm-3.5.2.src llvm
 
 function fatal
 {
@@ -13,12 +9,29 @@ function fatal
 }
 
 
-sudo mkdir -p /opt/llvm-release
-sudo chown $USER /opt/llvm-release
+rm -rf llvm-3.5.2.src
+rm -f llvm
+tar xJf llvm-3.5.2.src.tar.xz
+ln -f -s llvm-3.5.2.src llvm
+
+#sudo rm -rf /opt/llvm35-release+assert
+#sudo mkdir -p /opt/llvm35-release+assert
+#sudo chown $USER /opt/llvm35-release+assert
+#rm -rf build
+#mkdir -p build
+#(cd build &&
+#   ../llvm/configure --prefix=/opt/llvm35-release+assert \
+#	--enable-targets=x86_64 --enable-optimized=YES &&
+#   make clean && make -j8 && sudo make install) || fatal llvm-release+assert
+
+
+sudo rm -rf /opt/llvm35-release
+sudo mkdir -p /opt/llvm35-release
+sudo chown $USER /opt/llvm35-release
 rm -rf build
 mkdir -p build
 (cd build && 
-    ../llvm/configure --prefix=/opt/llvm-release \
+    ../llvm/configure --prefix=/opt/llvm35-release \
 	--enable-targets=x86_64 --enable-optimized=YES --enable-assertions=NO &&
-    make clean && make -j8 && sudo make install) || fatal llvm-release 
-
+    make clean && make -j8 && sudo make install) || fatal llvm-release
+ 
